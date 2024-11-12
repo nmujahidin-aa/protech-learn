a78651bc8f5a5c1ede4e4847167a35f692866d92
nmujahidin-aa <nurmujahidin18@gmail.com>
2024-11-12 14:11:03 +0700

    chore: change dashboard view

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -4,14 +4,31 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Team;
+use App\Models\Material;
 
 class DashboardController extends Controller
 {
     private $view;
+    private $user;
+    private $team;
+    private $material;
     public function __construct(){
         $this->view = "pages.admin.dashboard.";
+        $this->user = new User();
+        $this->team = new Team();
+        $this->material = new Material();
     }
     public function index(){
-        return view($this->view."index");
+        $user = $this->user->all();
+        $team = $this->team->all();
+        $material = $this->material->all();
+
+        return view($this->view."index",[
+            'user' => $user,
+            'team' => $team,
+            'material' => $material,
+        ]);
     }
 }
